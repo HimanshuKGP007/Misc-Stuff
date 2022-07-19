13a2e020e1cb8089f19a972230874dcf8d7e6e72
Himanshu <49796034+HimanshuKGP007@users.noreply.github.com>
2022-07-19 19:36:45 +0530

    Count of Null, None, NaN of All DataFrame Columns

---
+++ pyspark_nan.py
@@ -0,0 +1,3 @@
+# Find Count of Null, None, NaN of All DataFrame Columns
+from pyspark.sql.functions import col,isnan, when, count
+df.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in df.columns]).show()
