15ec94f5b9ae3419d12c523b5f5208bc422bcf46
Himanshu <49796034+HimanshuKGP007@users.noreply.github.com>
2022-07-19 20:12:58 +0530

    common cols excel

--- pyspark_nan.py
+++ pyspark_nan.py
@@ -1,3 +1,5 @@
 # Find Count of Null, None, NaN of All DataFrame Columns
 from pyspark.sql.functions import col,isnan, when, count
 df.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in df.columns]).show()
+
+=INDEX($B$3:$B$12, SMALL(IF(COUNTIF($D$3:$D$12, $B$3:$B$12), MATCH(ROW($B$3:$B$12),ROW($B$3:$B$12)), ""), ROWS($A$1:A1)))
