244d2922915b02bde9f60bc6fe35bfb76dfa6d22
Himanshu <49796034+HimanshuKGP007@users.noreply.github.com>
2022-07-26 16:03:17 +0530

    last and first dates of previous month

---
+++ Pyspark/last_first_dates.sql
@@ -0,0 +1,17 @@
+SQL> 
+SQL> select last_day(add_months(sysdate,-1)) from dual;
+
+LAST_DAY
+--------
+28.02.10
+
+SQL> select trunc(add_months(sysdate,-1),'MM') from dual
+
+TRUNC(AD
+--------
+01.02.10
+
+SQL>
+
+-- https://community.oracle.com/tech/developers/discussion/1050998/extract-first-and-last-date-of-previous-month
+-- https://stackoverflow.com/questions/45809743/best-practices-to-get-first-date-and-last-date-of-previous-month-in-oracle 
